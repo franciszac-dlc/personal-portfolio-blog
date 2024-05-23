293df76510909204957004ad0f16e30a78ac8d8e
Francis Zac <franciszac.dlc@gmail.com>
2024-05-23 23:44:37 +1000

    Favicon update

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import Image from './Image'
 
 const Header = () => {
   return (
@@ -13,10 +14,17 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Logo />
+              {/* <Logo /> */}
+              <Image
+                src="/static/images/logo.png"
+                alt="avatar"
+                width={192}
+                height={192}
+                className="h-16 w-16 rounded-full"
+              />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
+              <div className="hidden h-8 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
